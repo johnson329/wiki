87363f619a4bbda0a0ad4ea105a233277cf51d26
Your Name <you@example.com>
2021-02-07 17:56:25 +0800

    mvp

--- qaschool/settings.py
+++
@@ -1,9 +0,0 @@
-from .common import *
-
-DEBUG = True
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
-    }
-}
