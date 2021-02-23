9a543dca8eb6b4d95bde00ae9a70bd993a3e6587
Your Name <you@example.com>
2021-02-23 23:18:43 +0800

    后台标题

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Category, Course, Lesson
 
-admin.site.site_header = 'QA学院后台'
-admin.site.site_title = 'QA学院后台'
+admin.site.site_header = 'V2nwork'
+admin.site.site_title = 'V2nwork'
 
 
 class CategoryAdmin(admin.ModelAdmin):
